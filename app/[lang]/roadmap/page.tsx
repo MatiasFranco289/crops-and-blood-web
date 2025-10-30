c189d43588b93ce46706dce8e67e42d55d14b173
Deshens <matias.franco289@gmail.com>
2025-10-30 18:04:50 -0300

    now the roadmap is responsive and has a better look

--- app/[lang]/roadmap/page.tsx
+++ app/[lang]/roadmap/page.tsx
@@ -1,129 +1,307 @@
+"use client";
 import ObjetiveFlag from "@/app/components/ObjetiveFlag";
-import Separator from "@/app/components/Separator";
-import { MdCheckBoxOutlineBlank } from "react-icons/md";
+import { useRef } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import SimpleBar from "simplebar-react";
+import "simplebar-react/dist/simplebar.min.css";
 
 export default function RoadMap() {
-  const roadMap = [
-    {
-      phase: "Fase 1",
-      title: "Diseño preliminar",
-      startDate: "2025-10-24",
-      endDate: "2026-02-01",
-      objetives: [
-        {
-          name: "Creacion de la pagina web del juego",
-          fulfilled: true,
-        },
-        {
-          name: "Escritura y revision del GDD",
-          fulfilled: false,
-        },
-        {
-          name: "Creacion y estimacion de tareas",
-          fulfilled: false,
-        },
-        {
-          name: "Creacion del roadmap",
-          fulfilled: false,
-        },
-        {
-          name: "Busqueda del estilo artistico para el juego",
-          fulfilled: false,
-        },
-        {
-          name: "Renunciar al trabajo",
-          fulfilled: false,
-        },
-      ],
-    },
-    {
-      phase: "Fase 1",
-      title: "Diseño preliminar",
-      startDate: "2025-10-24",
-      endDate: "2026-02-01",
-      objetives: [
-        {
-          name: "Creacion de la pagina web del juego",
-          fulfilled: true,
-        },
-        {
-          name: "Escritura y revision del GDD",
-          fulfilled: false,
-        },
-        {
-          name: "Creacion y estimacion de tareas",
-          fulfilled: false,
-        },
-        {
-          name: "Creacion del roadmap",
-          fulfilled: false,
-        },
-        {
-          name: "Busqueda del estilo artistico para el juego",
-          fulfilled: false,
-        },
-        {
-          name: "Renunciar al trabajo",
-          fulfilled: false,
-        },
-      ],
-    },
-    {
-      phase: "Fase 1",
-      title: "Diseño preliminar",
-      startDate: "2025-10-24",
-      endDate: "2026-02-01",
-      objetives: [
-        {
-          name: "Creacion de la pagina web del juego",
-          fulfilled: true,
-        },
-        {
-          name: "Escritura y revision del GDD",
-          fulfilled: false,
-        },
-        {
-          name: "Creacion y estimacion de tareas",
-          fulfilled: false,
-        },
-        {
-          name: "Creacion del roadmap",
-          fulfilled: false,
-        },
-        {
-          name: "Busqueda del estilo artistico para el juego",
-          fulfilled: false,
-        },
-        {
-          name: "Renunciar al trabajo",
-          fulfilled: false,
-        },
-      ],
-    },
-  ];
+  const roadMap = {
+    version: "1.0.0",
+    lastUpdate: "2025-30-10",
+    phases: [
+      {
+        phase: "Fase 1",
+        title: "Diseño preliminar",
+        date: "2025-10-24",
+        completed: true,
+        objetives: [
+          {
+            name: "Creacion de la pagina web del juego",
+            fulfilled: true,
+          },
+          {
+            name: "Escritura y revision del GDD",
+            fulfilled: false,
+          },
+          {
+            name: "Creacion y estimacion de tareas",
+            fulfilled: false,
+          },
+          {
+            name: "Creacion del roadmap",
+            fulfilled: false,
+          },
+          {
+            name: "Busqueda del estilo artistico para el juego",
+            fulfilled: false,
+          },
+          {
+            name: "Renunciar al trabajo",
+            fulfilled: false,
+          },
+        ],
+      },
+      {
+        phase: "Fase 1",
+        title: "Diseño preliminar",
+        date: "2025-10-24",
+        completed: false,
+        objetives: [
+          {
+            name: "Creacion de la pagina web del juego",
+            fulfilled: true,
+          },
+          {
+            name: "Escritura y revision del GDD",
+            fulfilled: false,
+          },
+          {
+            name: "Creacion y estimacion de tareas",
+            fulfilled: false,
+          },
+          {
+            name: "Creacion del roadmap",
+            fulfilled: false,
+          },
+          {
+            name: "Busqueda del estilo artistico para el juego",
+            fulfilled: false,
+          },
+          {
+            name: "Renunciar al trabajo",
+            fulfilled: false,
+          },
+        ],
+      },
+      {
+        phase: "Fase 1",
+        title: "Diseño preliminar",
+        date: "2025-10-24",
+        completed: false,
+        objetives: [
+          {
+            name: "Creacion de la pagina web del juego",
+            fulfilled: true,
+          },
+          {
+            name: "Escritura y revision del GDD",
+            fulfilled: false,
+          },
+          {
+            name: "Creacion y estimacion de tareas",
+            fulfilled: false,
+          },
+          {
+            name: "Creacion del roadmap",
+            fulfilled: false,
+          },
+          {
+            name: "Busqueda del estilo artistico para el juego",
+            fulfilled: false,
+          },
+          {
+            name: "Renunciar al trabajo",
+            fulfilled: false,
+          },
+        ],
+      },
+      {
+        phase: "Fase 1",
+        title: "Diseño preliminar",
+        date: "2025-10-24",
+        completed: false,
+        objetives: [
+          {
+            name: "Creacion de la pagina web del juego",
+            fulfilled: true,
+          },
+          {
+            name: "Escritura y revision del GDD",
+            fulfilled: false,
+          },
+          {
+            name: "Creacion y estimacion de tareas",
+            fulfilled: false,
+          },
+          {
+            name: "Creacion del roadmap",
+            fulfilled: false,
+          },
+          {
+            name: "Busqueda del estilo artistico para el juego",
+            fulfilled: false,
+          },
+          {
+            name: "Renunciar al trabajo",
+            fulfilled: false,
+          },
+        ],
+      },
+      {
+        phase: "Fase 1",
+        title: "Diseño preliminar",
+        date: "2025-10-24",
+        completed: false,
+        objetives: [
+          {
+            name: "Creacion de la pagina web del juego",
+            fulfilled: true,
+          },
+          {
+            name: "Escritura y revision del GDD",
+            fulfilled: false,
+          },
+          {
+            name: "Creacion y estimacion de tareas",
+            fulfilled: false,
+          },
+          {
+            name: "Creacion del roadmap",
+            fulfilled: false,
+          },
+          {
+            name: "Busqueda del estilo artistico para el juego",
+            fulfilled: false,
+          },
+          {
+            name: "Renunciar al trabajo",
+            fulfilled: false,
+          },
+        ],
+      },
+      {
+        phase: "Fase 1",
+        title: "Diseño preliminar",
+        date: "2025-10-24",
+        completed: false,
+        objetives: [
+          {
+            name: "Creacion de la pagina web del juego",
+            fulfilled: true,
+          },
+          {
+            name: "Escritura y revision del GDD",
+            fulfilled: false,
+          },
+          {
+            name: "Creacion y estimacion de tareas",
+            fulfilled: false,
+          },
+          {
+            name: "Creacion del roadmap",
+            fulfilled: false,
+          },
+          {
+            name: "Busqueda del estilo artistico para el juego",
+            fulfilled: false,
+          },
+          {
+            name: "Renunciar al trabajo",
+            fulfilled: false,
+          },
+        ],
+      },
+      {
+        phase: "Fase 1",
+        title: "Diseño preliminar",
+        date: "2025-10-24",
+        completed: false,
+        objetives: [
+          {
+            name: "Creacion de la pagina web del juego",
+            fulfilled: true,
+          },
+          {
+            name: "Escritura y revision del GDD",
+            fulfilled: false,
+          },
+          {
+            name: "Creacion y estimacion de tareas",
+            fulfilled: false,
+          },
+          {
+            name: "Creacion del roadmap",
+            fulfilled: false,
+          },
+          {
+            name: "Busqueda del estilo artistico para el juego",
+            fulfilled: false,
+          },
+          {
+            name: "Renunciar al trabajo",
+            fulfilled: false,
+          },
+        ],
+      },
+      {
+        phase: "Fase 1",
+        title: "Diseño preliminar",
+        date: "2025-10-24",
+        completed: false,
+        objetives: [
+          {
+            name: "Creacion de la pagina web del juego",
+            fulfilled: true,
+          },
+          {
+            name: "Escritura y revision del GDD",
+            fulfilled: false,
+          },
+          {
+            name: "Creacion y estimacion de tareas",
+            fulfilled: false,
+          },
+          {
+            name: "Creacion del roadmap",
+            fulfilled: false,
+          },
+          {
+            name: "Busqueda del estilo artistico para el juego",
+            fulfilled: false,
+          },
+          {
+            name: "Renunciar al trabajo",
+            fulfilled: false,
+          },
+          {
+            name: "Creacion del roadmap",
+            fulfilled: false,
+          },
+        ],
+      },
+    ],
+  };
 
   return (
-    <div className="font-roboto bg-[#12050d] min-h-screen w-full">
-      <div className="flex flex-col items-center">
-        <h2 className="pt-24 text-4xl font-semibold text-center">
-          Roadmap V1.0
+    <div className="font-roboto bg-[#12050d] flex flex-col items-center min-h-screen overflow-hidden">
+      <div className="flex flex-col items-center mt-20">
+        <h2 className="text-3xl sm:text-4xl font-semibold text-center underline">
+          {`Roadmap V${roadMap.version}`}
         </h2>
-        <Separator width="w-4/6" gap="h-16" />
+        <h3 className="text-white/80">Last update 2025-30-10</h3>
       </div>
 
-      <div className="w-full flex justify-between px-40 mt-12 ">
-        {roadMap.map((flag, index) => {
-          return (
-            <ObjetiveFlag
-              title={flag.title}
-              phase={flag.phase}
-              startDate={flag.startDate}
-              endDate={flag.endDate}
-              objetives={flag.objetives}
-              key={`flag_${index}`}
-            />
-          );
-        })}
+      <div className="w-full flex justify-center mt-8">
+        <div className="w-full sm:w-5/6">
+          <SimpleBar autoHide={false} style={{ maxHeight: "none" }}>
+            <div className="flex space-x-0 sm:space-x-12 sm:flex-row flex-col overflow-x-hidden sm:overflow-x-visible">
+              {roadMap.phases.map((flag, index) => {
+                return (
+                  <ObjetiveFlag
+                    title={flag.title}
+                    phase={flag.phase}
+                    date={flag.date}
+                    objetives={flag.objetives}
+                    key={`flag_${index}`}
+                    drawTimeline={index !== roadMap.phases.length - 1}
+                    completed={flag.completed}
+                  />
+                );
+              })}
+            </div>
+          </SimpleBar>
+        </div>
       </div>
     </div>
   );
--- app/components/ObjetiveFlag.tsx
+++ app/components/ObjetiveFlag.tsx
@@ -1,90 +1,90 @@
-import { MdCheckBoxOutlineBlank } from "react-icons/md";
+import { forwardRef, RefObject } from "react";
+import { MdCheckBoxOutlineBlank, MdOutlineCheckBox } from "react-icons/md";
 import { TbPointFilled } from "react-icons/tb";
+import Separator from "./Separator";
+import { getMonthName } from "../utils";
+import { useParams } from "next/navigation";
 
 interface ObjetiveFlagProps {
-  startDate: string;
-  endDate: string;
+  date: string;
   phase: string;
   title: string;
   objetives: Array<{ name: string; fulfilled: boolean }>;
+  ref: RefObject<HTMLDivElement | null>;
+  drawTimeline?: boolean;
+  completed: boolean;
 }
 
-export default function ObjetiveFlag({
-  startDate,
-  endDate,
-  phase,
-  title,
-  objetives,
-}: ObjetiveFlagProps) {
-  return (
-    <div className="flex flex-col items-center">
-      <TbPointFilled className="text-2xl" />
-      <h2 className="text-xl font-semibold my-3">Ene. 2025</h2>
-      <div className="w-[500px] flex flex-col items-center relative">
-        <img
-          src={"/stick2.png"}
-          className="w-full z-30"
-          style={{ imageRendering: "pixelated" }}
-        />
+const ObjetiveFlag = forwardRef<HTMLDivElement, ObjetiveFlagProps>(
+  ({ date, phase, title, objetives, drawTimeline = false, completed }, ref) => {
+    const lang = useParams().lang as "en" | "es";
+    const monthName = getMonthName(date, lang);
+    const year = date.split("T")[0].split("-")[0];
 
-        <div className="w-8/12 absolute top-10 z-0 bg-[#180712]  p-6">
+    return (
+      <div className="flex flex-col items-center font-roboto mb-42" ref={ref}>
+        <div className="flex flex-col-reverse sm:flex-col items-center mb-2 space-y-2 ml-4 sm:ml-0 w-full">
+          <h3 className="text-xl font-semibold text-center">{`${monthName}. ${year}`}</h3>
+          <div className="sm:hidden flex w-full justify-center">
+            <Separator gap="h-4" width="w-4/6" />
+          </div>
+          <div className="z-50">
+            <TbPointFilled
+              className={`text-4xl block ${
+                completed ? "text-[#8b1431]" : "text-white"
+              }`}
+            />
+          </div>
+        </div>
+
+        <div className="w-full sm:max-w[1000px] max-w-[420px] sm:w-[400px] lg:w-[500px] h-full relative flex justify-center">
+          {drawTimeline && (
+            <div
+              className={`h-[3px] ${
+                completed ? "bg-[#8b1431]" : "bg-white"
+              } min-w-[calc(100%+42px)] absolute top-[-28] left-[50%] hidden sm:block z-10`}
+            />
+          )}
           <img
-            src={"/flag.png"}
-            className="w-full absolute top-full left-0"
+            src={"/stick2.png"}
+            className="w-full absolute top-0 z-30"
             style={{ imageRendering: "pixelated" }}
           />
 
-          <div>
-            <div className="flex flex-col items-center mt-3">
-              <h3 className="text-lg underline">{phase}</h3>
-              <h3 className="text-xl font-semibold">{title}</h3>
-            </div>
-          </div>
+          {/* Card */}
+          <div className="bg-[#180712] mt-6 w-9/12 sm:8/12 flex flex-col items-center p-4 relative z-20">
+            <h3 className="mt-8 text-lg text-center underline">{phase}</h3>
+            <h3 className="text-xl font-semibold text-center">{title}</h3>
 
-          <div className="my-6 space-y-2">
-            {objetives.map((objetive, index) => {
-              return (
-                <div
-                  className="flex items-center justify-start"
-                  key={`active_objetive_${phase}_${index}`}
-                >
-                  <MdCheckBoxOutlineBlank className="mr-3 shrink-0 text-xl" />
-                  <p>{objetive.name}</p>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-
-        <div className="w-8/12 top-10 z-0 bg-[#180712] p-6 invisible">
-          <div>
-            <div className="flex justify-center text-white/50">
-              <p>{startDate}</p>
-              <p className="mx-2">-</p>
-              <p>{endDate}</p>
+            {/* List */}
+            <div className="mt-4 space-y-2">
+              {objetives.map((objetive, index) => {
+                return (
+                  <div
+                    key={`phase_${phase}_objetive_${index}`}
+                    className="flex items-center"
+                  >
+                    {objetive.fulfilled ? (
+                      <MdOutlineCheckBox className="text-2xl flex-shrink-0" />
+                    ) : (
+                      <MdCheckBoxOutlineBlank className="text-2xl flex-shrink-0" />
+                    )}
+                    <p className="ml-2">{objetive.name}</p>
+                  </div>
+                );
+              })}
             </div>
 
-            <div className="flex flex-col items-center mt-3">
-              <h3 className="text-lg underline">{phase}</h3>
-              <h3 className="text-xl font-semibold">{title}</h3>
-            </div>
-          </div>
-
-          <div className="my-6 space-y-2">
-            {objetives.map((objetive, index) => {
-              return (
-                <div
-                  className="flex items-center justify-start"
-                  key={`invisible_objetive_${phase}_${index}`}
-                >
-                  <MdCheckBoxOutlineBlank className="mr-3 shrink-0 text-xl" />
-                  <p>{objetive.name}</p>
-                </div>
-              );
-            })}
+            <img
+              src={"/flag.png"}
+              className=" w-full top-full absolute"
+              style={{ imageRendering: "pixelated" }}
+            />
           </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
+
+export default ObjetiveFlag;
